2afdb7af1f1ab47d28c02504ea32d40fe635b6cf
Reece Crook <reececrook16@gmail.com>
2023-08-02 16:11:10 -0600

    Completed Lab

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    evens = [number for number in num_list if (number / 2).is_integer()]
+    return evens
 
 def make_exclamation(sentence_list):
-    pass
+    new_list = [f"{sentence}!" for sentence in sentence_list]
+    return new_list
